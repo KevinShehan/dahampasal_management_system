88d3d34a5b86cbd8a5323fec11d05c82501540e6
Kevin <kevin.shehan30@gmail.com>
2024-08-17 21:08:03 +0530

    alphine js added

--- resources/views/_layouts/app_view.blade.php
+++ resources/views/_layouts/app_view.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Student Management System</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.9.1/dist/cdn.min.js" defer></script>
     <style>
       @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
       body {
