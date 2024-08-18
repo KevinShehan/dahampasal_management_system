de5392a454bf70092075e2f3f1e061e70c9f2d79
Kevin <kevin.shehan30@gmail.com>
2024-08-18 12:55:49 +0530

    Dynamic title added

--- resources/views/_layouts/app_view.blade.php
+++ resources/views/_layouts/app_view.blade.php
@@ -5,10 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Student Management System</title>
+    <title>{{ config('app.name', 'Church Management System') }}</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.9.1/dist/cdn.min.js" defer></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="icon" type="image/x-icon" href="{{ asset('images/fav.png') }}">
     {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
     @livewireStyles
     <style>
