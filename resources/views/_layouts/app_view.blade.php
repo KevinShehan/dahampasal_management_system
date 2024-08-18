25f49898f8e94ea00e6db4e8245afdfe56743aa5
Kevin <kevin.shehan30@gmail.com>
2024-08-18 12:58:09 +0530

    structured

--- resources/views/_layouts/app_view.blade.php
+++ resources/views/_layouts/app_view.blade.php
@@ -13,14 +13,17 @@
     {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
     @livewireStyles
     <style>
-      @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
-      body {
-        font-family: 'Inter', sans-serif;
-      }
+        @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
+
+        body {
+            font-family: 'Inter', sans-serif;
+        }
     </style>
 </head>
+
 <body>
     @yield('content')
     @livewireScripts
 </body>
+
 </html>
