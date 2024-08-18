02088114bdcac78b1e64343a72b0d800c014ab35
Kevin <kevin.shehan30@gmail.com>
2024-08-18 13:17:00 +0530

    web rout logout

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,11 @@ Route::get('/academics', function () { return view('pages/academic');});
 Route::get('/users', function () { return view('pages/user_view');});
 Route::get('/addstudent', function () { return view('pages/Student_create');});
 Route::get('/viewstudents', function () { return view('pages/Students_view');});
+Route::get('/profile', function () { return view('pages/Profile');});
+Route::get('/settings', function () { return view('pages/Settings');});
+Route::get('/logout', function () {
+    // Destroy the session
+    session()->flush();
+    // Redirect to the login page
+    return redirect('/login');
+})->name('logout');
