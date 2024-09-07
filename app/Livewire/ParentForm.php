c73598ae516b202e1819bc6c8f1fd6e82a5bd60e
Kevin <kevin.shehan30@gmail.com>
2024-09-07 14:08:46 +0530

    Teacher parent livewire component created

---
+++ app/Livewire/ParentForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ParentForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.parent-form');
+    }
+}
